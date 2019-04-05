be1a7dcbe24a82ceac642c3ef09519e52eba8a1c
royer <ing.royerhernandez@gmail.com>
2019-04-04 23:18:41 -0500

    Juego del Ahorcado

---
+++ Ahorcado/ahorcado/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace ahorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.ptbCuarto = new System.Windows.Forms.PictureBox();
+            this.ptbPrimero = new System.Windows.Forms.PictureBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnInsertarLetra = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLetras = new System.Windows.Forms.TextBox();
+            this.txtPalabras = new System.Windows.Forms.TextBox();
+            this.ptbTercero = new System.Windows.Forms.PictureBox();
+            this.ptbSegundo = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbCuarto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrimero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbTercero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSegundo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(381, 292);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(130, 23);
+            this.btnRegresar.TabIndex = 85;
+            this.btnRegresar.Text = "Regresar al menu";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // ptbCuarto
+            // 
+            this.ptbCuarto.Image = ((System.Drawing.Image)(resources.GetObject("ptbCuarto.Image")));
+            this.ptbCuarto.Location = new System.Drawing.Point(14, 370);
+            this.ptbCuarto.Name = "ptbCuarto";
+            this.ptbCuarto.Size = new System.Drawing.Size(280, 112);
+            this.ptbCuarto.TabIndex = 83;
+            this.ptbCuarto.TabStop = false;
+            // 
+            // ptbPrimero
+            // 
+            this.ptbPrimero.Image = ((System.Drawing.Image)(resources.GetObject("ptbPrimero.Image")));
+            this.ptbPrimero.Location = new System.Drawing.Point(30, 24);
+            this.ptbPrimero.Name = "ptbPrimero";
+            this.ptbPrimero.Size = new System.Drawing.Size(247, 111);
+            this.ptbPrimero.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbPrimero.TabIndex = 80;
+            this.ptbPrimero.TabStop = false;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(478, 194);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(19, 13);
+            this.label20.TabIndex = 79;
+            this.label20.Text = "__";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(458, 194);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(19, 13);
+            this.label19.TabIndex = 78;
+            this.label19.Text = "__";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(438, 194);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(19, 13);
+            this.label18.TabIndex = 77;
+            this.label18.Text = "__";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(418, 194);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(19, 13);
+            this.label17.TabIndex = 76;
+            this.label17.Text = "__";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(398, 194);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(19, 13);
+            this.label16.TabIndex = 75;
+            this.label16.Text = "__";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(378, 194);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(19, 13);
+            this.label15.TabIndex = 74;
+            this.label15.Text = "__";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(358, 194);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(19, 13);
+            this.label14.TabIndex = 73;
+            this.label14.Text = "__";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(338, 194);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(19, 13);
+            this.label13.TabIndex = 72;
+            this.label13.Text = "__";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(318, 194);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 71;
+            this.label12.Text = "__";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(298, 194);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 70;
+            this.label11.Text = "__";
+            // 
+            // btnInsertarLetra
+            // 
+            this.btnInsertarLetra.Location = new System.Drawing.Point(301, 245);
+            this.btnInsertarLetra.Name = "btnInsertarLetra";
+            this.btnInsertarLetra.Size = new System.Drawing.Size(106, 23);
+            this.btnInsertarLetra.TabIndex = 69;
+            this.btnInsertarLetra.Text = "Insertar Letra";
+            this.btnInsertarLetra.UseVisualStyleBackColor = true;
+            this.btnInsertarLetra.Click += new System.EventHandler(this.btnInsertarLetra_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(478, 181);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 68;
+            this.label10.Text = "X";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(458, 181);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 13);
+            this.label9.TabIndex = 67;
+            this.label9.Text = "X";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(438, 181);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 13);
+            this.label8.TabIndex = 66;
+            this.label8.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(418, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 65;
+            this.label7.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(398, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 64;
+            this.label6.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(378, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 63;
+            this.label5.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(358, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 62;
+            this.label4.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 61;
+            this.label3.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 60;
+            this.label2.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 59;
+            this.label1.Text = "X";
+            // 
+            // txtLetras
+            // 
+            this.txtLetras.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtLetras.Location = new System.Drawing.Point(418, 244);
+            this.txtLetras.Name = "txtLetras";
+            this.txtLetras.Size = new System.Drawing.Size(100, 20);
+            this.txtLetras.TabIndex = 58;
+            // 
+            // txtPalabras
+            // 
+            this.txtPalabras.Enabled = false;
+            this.txtPalabras.Location = new System.Drawing.Point(301, 218);
+            this.txtPalabras.Name = "txtPalabras";
+            this.txtPalabras.Size = new System.Drawing.Size(217, 20);
+            this.txtPalabras.TabIndex = 57;
+            // 
+            // ptbTercero
+            // 
+            this.ptbTercero.Image = ((System.Drawing.Image)(resources.GetObject("ptbTercero.Image")));
+            this.ptbTercero.Location = new System.Drawing.Point(14, 289);
+            this.ptbTercero.Name = "ptbTercero";
+            this.ptbTercero.Size = new System.Drawing.Size(233, 85);
+            this.ptbTercero.TabIndex = 82;
+            this.ptbTercero.TabStop = false;
+            // 
+            // ptbSegundo
+            // 
+            this.ptbSegundo.Image = ((System.Drawing.Image)(resources.GetObject("ptbSegundo.Image")));
+            this.ptbSegundo.Location = new System.Drawing.Point(15, 131);
+            this.ptbSegundo.Name = "ptbSegundo";
+            this.ptbSegundo.Size = new System.Drawing.Size(232, 158);
+            this.ptbSegundo.TabIndex = 81;
+            this.ptbSegundo.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(301, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(196, 106);
+            this.pictureBox1.TabIndex = 86;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 488);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.ptbCuarto);
+            this.Controls.Add(this.ptbPrimero);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnInsertarLetra);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLetras);
+            this.Controls.Add(this.txtPalabras);
+            this.Controls.Add(this.ptbTercero);
+            this.Controls.Add(this.ptbSegundo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbCuarto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbPrimero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbTercero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSegundo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.PictureBox ptbCuarto;
+        private System.Windows.Forms.PictureBox ptbPrimero;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnInsertarLetra;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLetras;
+        private System.Windows.Forms.TextBox txtPalabras;
+        private System.Windows.Forms.PictureBox ptbTercero;
+        private System.Windows.Forms.PictureBox ptbSegundo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
